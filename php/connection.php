54c43ecf1fc637b0f289cd4f9c3e1f66c5f10deb
ihategoto <cdemilio80@gmail.com>
2019-03-10 22:04:54 +0100

    Specificate le query di creazione nei commenti

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,11 @@
 <?php
 /*
     Questo effettua la connessione con il database.
+
+    Query utilizzate per la creazione:
+    CREATE TABLE autori (id INT NOT NULL AUTO_INCREMENT, Nome TINYTEXT NOT NULL, Cognome TINYTEXT NOT NULL, Ritratto TINYTEXT NOT NULL, DataN DATE NOT NULL, PRIMARY KEY (id));
+    CREATE TABLE tecniche (id INT NOT NULL AUTO_INCREMENT, nomeT VARCHAR(30) NOT NULL, PRIMARY KEY (id));
+    CREATE TABLE quadri (id INT NOT NULL AUTO_INCREMENT, Titolo VARCHAR(50) NOT NULL, DataP DATE NOT NULL, Autore INT NOT NULL, Tecnica INT NOT NULL, PRIMARY KEY  (id), FOREIGN KEY  (Autore) REFERENCES autori(id), FOREIGN KEY  (Tecnica) REFERENCES tecniche(id));
 */
 
 $conn = new mysqli("localhost", "root", "D3005Chh77", "Galleria");
